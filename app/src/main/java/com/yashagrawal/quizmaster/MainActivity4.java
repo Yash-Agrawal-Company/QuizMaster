e18fc5eef115540ff6e3a2a34f14486a80cc05d0
Yash Agrawal <yashagrawal9319029562@gmail.com>
2023-04-05 20:03:51 -0700

    Quiz master -> login checking added

--- app/src/main/java/com/yashagrawal/quizmaster/MainActivity4.java
+++ app/src/main/java/com/yashagrawal/quizmaster/MainActivity4.java
@@ -3,6 +3,7 @@ package com.yashagrawal.quizmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+//        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+    }
+
+    private void setSupportActionBar(Toolbar myToolbar) {
     }
 }
